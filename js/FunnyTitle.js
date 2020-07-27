5b0ad70e82aa77f5549c192a7e00b9b2aa1a2054
demon_liu <liu2985374516@gmail.com>
2020-07-27 21:08:08 +0800

    Site updated: 2020-07-27 21:08:06

--- js/FunnyTitle.js
+++ js/FunnyTitle.js
@@ -4,12 +4,12 @@
  document.addEventListener('visibilitychange', function () {
      if (document.hidden) {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヽ(●-`Д´-)ノ被忘记了啊！';
+         document.title = 'ヽ(●-`Д´-)ノ又要一个人了啊。';
          clearTimeout(titleTime);
      }
      else {
          $('[rel="icon"]').attr('href', "/img/trhx2.png");
-         document.title = 'ヾ(Ő∀Ő3)ノ没想到还会回来！' + OriginTitle;
+         document.title = 'ヾ(Ő∀Ő3)ノ很高兴你回来了！' + OriginTitle;
          titleTime = setTimeout(function () {
              document.title = OriginTitle;
          }, 2000);
